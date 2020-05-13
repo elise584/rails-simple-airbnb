5eef5d19ef69ee559a9ad218958561cac72a06ee
Elise Dindault <elise.dindault@gmail.com>
2020-05-13 16:29:46 +0200

    index and new ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Romantic Flat in Paris',
+  address: '12 Rue de Hauteville, 75010 Paris',
+  description: 'A little cocoon for romantic escapade. One double bedroom, kitchenette and bathroom with jacuzzi',
+  price_per_night: 90,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Villa in Cancun',
+  address: 'Boulevard Kukulcan KM 16.5 L-48 Mza.53, Cancun, Quintana',
+  description: 'A beautiful villa with luxury prestations, close to the beach',
+  price_per_night: 50,
+  number_of_guests: 6
+)
+
+Flat.create!(
+  name: 'House by the sea in Norway',
+  address: 'Peder Larsen Karl Johansgate 25 B 0025 OSLO NORWAY',
+  description: 'A lovely little fisherman caban close to the sea. Typical decorum',
+  price_per_night: 54,
+  number_of_guests: 5
+)
